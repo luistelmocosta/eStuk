9f6f76937c1ade1b03b38f3e57c3def6ada2417f
LuísCosta <luistelmocosta@gmail.com>
2015-02-17 05:33:48 +0000

    Made the first sale

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ActiveRecord::Base
 
 
 	belongs_to :user
-	has many :sales
+	has_many :sales
 	has_attached_file :image
 	has_attached_file :resource
 
@@ -19,6 +19,6 @@ class Book < ActiveRecord::Base
 	 
 	validates :image, attachment_presence: true
 	validates :resource, attachment_presence: true 
-	validates_numerically_of :price, greater_than: 49, message: "must be atleast 50 cents"
+	validates_numericality_of :price, greater_than: 49, message: "must be atleast 50 cents"
 
 end 
