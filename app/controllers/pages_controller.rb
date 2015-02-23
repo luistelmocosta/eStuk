07b884743d1514cf7e542b11269da149e70e5d8b
LuísCosta <luistelmocosta@gmail.com>
2015-02-23 03:40:51 +0000

    Finished dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,12 @@ class PagesController < ApplicationController
 	def home
 		if current_user
 			redirect_to books_path
-			end
-			@books = Book.last(4)
 		end
-		def dashboard
-			@books = current_user.books
-			end
-		end
-  
+		@books = Book.last(4)
+	end
+	def dashboard
+		@books = current_user.books
+		@purchased = Sale.where(buyer_email: current_user.email)
+		@sales = Sale.where(seller_email: current_user.email)
+	end 
+end
